211063b3daed3b42286d2ea3ca6686be67f6e52b
David Banks <dave@hoglet.com>
2017-09-18 14:37:20 +0100

    simulation: moved top level test bench to Test directory
    Change-Id: Ifd696f61991d94569c58c86307c986e1cf41ffc2

--- blackice/simulate.sh
+++ blackice/simulate.sh
@@ -2,6 +2,6 @@
 
 SRCS="../src/bootstrap.v ../src/Microcomputer/Microcomputer.v ../src/ROMS/ROM.v ../src/Components/SDCARD/sd_controller.v ../src/Components/UART/bufferedUART.v ../src/Components/Z80/tv80_core.v ../src/Components/Z80/tv80n.v ../src/Components/Z80/tv80_mcode.v ../src/Components/Z80/tv80_reg.v ../src/Components/Z80/tv80_alu.v ../src/Components/TERMINAL/DisplayRam.v ../src/Components/TERMINAL/SBCTextDisplayRGB.v "
 
-iverilog ../src/cpmz80_tb.v $SRCS
+iverilog ../src/Test/cpmz80_tb.v $SRCS
 ./a.out  
 gtkwave -g -a signals.gtkw dump.vcd
